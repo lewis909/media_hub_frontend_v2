02ce0a1ca6a282c7966ee9ca22d83702f29156cc
lewis909 <lewis.dt@gmail.com>
2017-02-08 14:32:41 +0000

    created submit view and populated select drop down with options

--- submit/urls.py
+++ submit/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from . import views
+from django.views.generic import ListView
+from asset_db.models import Profiles
 
 urlpatterns = [
-    url(r'^$', views.submit, name='submit'),
+    url(r'^$', ListView.as_view(queryset=Profiles.objects.all().order_by("id"), template_name='submit/submit.html')),
 ]
