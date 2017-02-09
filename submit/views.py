246fc4c8bdf5c5fb0b4f1fd8da9deead240e13f7
lewis909 <lewis.dt@gmail.com>
2017-02-09 12:07:37 +0000

    commit POST work

--- submit/views.py
+++ submit/views.py
@@ -10,5 +10,13 @@ def submit(request):
 
 def success(request):
     profiles = Profiles.objects.order_by('-id')
-    context = {'profiles': profiles}
+    matid = request.POST['material_id']
+    workflow = request.POST['workflow']
+    start_date = request.POST['start_date']
+    end_date = request.POST['end_date']
+    context = {'profiles': profiles,
+               'matid': matid,
+               'workflow': workflow,
+               'start_date': start_date,
+               'end_date': end_date}
     return render(request, 'submit/success.html', context)
