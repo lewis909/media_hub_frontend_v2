fdd112186c43b8a4e940d6995e0ff69f77980849
lewis909 <lewis.dt@gmail.com>
2017-02-10 14:35:08 +0000

    update to forms

--- submit/forms.py
+++ submit/forms.py
@@ -6,6 +6,6 @@ class SubmitJob(forms.Form):
 
     material_id = forms.CharField(max_length=8)
     workflow = forms.ModelChoiceField(queryset=Profiles.objects.distinct('name'), to_field_name='name')
-    start_datepicker = forms.CharField(max_length=12)
-    end_datepicker = forms.CharField(max_length=12)
+    start_datepicker = forms.CharField(max_length=12, widget=forms.DateInput(attrs={'class': 'datepicker'}))
+    end_datepicker = forms.CharField(max_length=12, widget=forms.DateInput(attrs={'class': 'datepicker'}))
 
