974bf8fac3ad7a624f958a132863077f85a198d0
lewis909 <lewis.dt@gmail.com>
2017-02-10 11:31:44 +0000

    built Task DB insert

--- submit/forms.py
+++ submit/forms.py
@@ -5,7 +5,7 @@ from asset_db.models import Profiles
 class SubmitJob(forms.Form):
 
     material_id = forms.CharField(max_length=8)
-    workflow = forms.ModelChoiceField(queryset=Profiles.objects.all())
+    workflow = forms.ModelChoiceField(queryset=Profiles.objects.distinct('name'))
     start_datepicker = forms.CharField(max_length=12)
     end_datepicker = forms.CharField(max_length=12)
 
--- submit/views.py
+++ submit/views.py
@@ -1,4 +1,4 @@
-from asset_db.models import Profiles
+from asset_db.models import Profiles, Task
 from django.shortcuts import render_to_response, render
 
 from .forms import SubmitJob
@@ -7,11 +7,15 @@ from .forms import SubmitJob
 def job(request):
     if request.method == 'POST':
         form = SubmitJob(request.POST)
-
-        if(form.is_valid()):
+        if form.is_valid():
+            insert = Task()
+            insert.material_id = str(request.POST['material_id'])
+            insert.workflow = str(request.POST['workflow'])
+            insert.save()
             print(request.POST['material_id'])
             print(request.POST['workflow'])
             message = 'success'
+
         else:
             message = 'fail'
         return render(request, 'submit/submit.html', {'message': message})
