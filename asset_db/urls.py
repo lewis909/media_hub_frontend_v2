a996ba570dd446808d0179b19cb381794e9fa26d
lewis909 <lewis.dt@gmail.com>
2017-02-10 11:42:34 +0000

    set Task Status page to descending view

--- asset_db/urls.py
+++ asset_db/urls.py
@@ -4,5 +4,5 @@ from asset_db.models import AssetMetadata, Task
 
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=AssetMetadata.objects.all().order_by("id")[:10], template_name='assets_db/assets.html')),
-    url(r'^task/$', ListView.as_view(queryset=Task.objects.all().order_by("id")[:10], template_name='assets_db/task.html',), name='task')
+    url(r'^task/$', ListView.as_view(queryset=Task.objects.all().order_by("-id")[:10], template_name='assets_db/task.html',), name='task')
 ]
