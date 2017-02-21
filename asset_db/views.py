aa853c0a51511b85f16c877d3821e2ea40745b12
lewis909 <lewis.dt@gmail.com>
2017-02-21 11:02:15 +0000

    refined asset_db views and url. Updated paginate for task view.

--- asset_db/views.py
+++ asset_db/views.py
@@ -1,6 +1,42 @@
-from .models import AssetMetadata
+from .models import AssetMetadata, Task
 from django.views.generic import DetailView
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.decorators import login_required
 
 
 class AssetView(DetailView):
     model = AssetMetadata
+
+
+@login_required(login_url='login')
+def task_status(request):
+
+    task_list = Task.objects.order_by('-id').all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(task_list, 50)
+
+    try:
+        task = paginator.page(page)
+    except PageNotAnInteger:
+        task = paginator.page(1)
+    except EmptyPage:
+        task = paginator.page(paginator.num_pages)
+    return render(request, 'assets_db/task.html', {'task_status': task})
+
+
+@login_required(login_url='login')
+def assets_in_repo(request):
+
+    asset_list = AssetMetadata.objects.order_by('id').all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(asset_list, 50)
+
+    try:
+        assets = paginator.page(page)
+    except PageNotAnInteger:
+        assets = paginator.page(1)
+    except EmptyPage:
+        assets = paginator.page(paginator.num_pages)
+
+    return render(request, 'assets_db/assets.html', {'assets': assets})
