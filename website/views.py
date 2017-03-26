f0791efb68bc2a4f77aeaf3631cbcf0ff60e17ba
lewis909 <lewis.dt@gmail.com>
2017-03-26 10:46:16 +0100

    Aded docstrings for each function.

--- website/views.py
+++ website/views.py
@@ -6,6 +6,10 @@ from asset_db.models import Task
 
 @login_required(login_url='login')
 def index(request):
+    """ 
+        index creates the Home page, rendering data from the system blog,
+        it also renders the most recent 20 submitted jobs.
+    """
     blog_v = SystemBlog.objects.all().order_by('-id')[:5]
     task_list = Task.objects.order_by('-id')[:20].all()
     return render(request, 'website/home.html', {'user': request.user.username,
@@ -15,5 +19,8 @@ def index(request):
 
 @login_required(login_url='login')
 def index_update(request):
+    """
+        index_update creates the task data which the Home page template poll's using Jquery .load(). 
+    """
     task_list = Task.objects.order_by('-id')[:20].all()
     return render(request, 'website/home_update.html', {'user': request.user.username, 'task_list': task_list})
