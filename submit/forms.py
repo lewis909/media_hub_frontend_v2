02d39187d79944bd2c0d6f9b8cb087eeee8561ca
lewis909 <lewis.dt@gmail.com>
2017-03-26 10:58:43 +0100

    Added Docstring

--- submit/forms.py
+++ submit/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from asset_db.models import Profiles, AssetMetadata
+from asset_db.models import Profiles
 
 
 class SubmitJob(forms.Form):
-
+    """ Form used to submit job to media_hub back end."""
     material_id = forms.CharField(max_length=8, widget=forms.TextInput(attrs={'class': 'autocomplete'}))
     workflow = forms.ModelChoiceField(queryset=Profiles.objects.distinct('name'), to_field_name='name')
     start_datepicker = forms.CharField(max_length=12, widget=forms.DateInput(attrs={'class': 'datepicker'}))
