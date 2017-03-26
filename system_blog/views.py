572cea6b35f248a871e11b888b5449dc62ef13f6
lewis909 <lewis.dt@gmail.com>
2017-03-26 10:49:23 +0100

    Added docstring

--- system_blog/views.py
+++ system_blog/views.py
@@ -5,6 +5,10 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def blog(request):
+    """
+        Blog renders data from the blog DB table which is being used to provide system updates to users.
+        This information is rendered on the Home page and all posts can be seen at /blog/ url.
+    """
 
     sys_blog = SystemBlog.objects.all().order_by('-id')
     context = {'blog': sys_blog}
